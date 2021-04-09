3f2121e922534e8cd2e12f567a8b62b2f014340b
FaishalSatria <82191716+FaishalSatria@users.noreply.github.com>
2021-04-09 18:20:29 +0700

    Update tambahtag.php

--- admin/tambahtag.php
+++ admin/tambahtag.php
@@ -42,14 +42,22 @@
       <!-- form start -->
       </br>
       <div class="col-sm-10">
+      <?php if (!empty($_GET['notif'])) {?>
+              <?php  if ($_GET['notif'] == "tambahkosong") {?>
           <div class="alert alert-danger" role="alert">Maaf data tag wajib di isi</div>
+          <?php }?>
+              <?php }?>
       </div>
-      <form class="form-horizontal">
+      <form class="form-horizontal" method="post" action="konfirmasitambahtag.php">
         <div class="card-body">
           <div class="form-group row">
             <label for="tag" class="col-sm-3 col-form-label">Tag</label>
             <div class="col-sm-7">
-              <input type="text" class="form-control" id="tag" value="">
+              <input type="text" class="form-control" id="tag" value="" name="tag">
+              
+              <!--<div class="alert alert-danger" role="alert">
+              maaf data tag wajib diisi</div>-->
+             
             </div>
           </div>
         </div>
